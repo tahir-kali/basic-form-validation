d08fb40a83df54603d48ffc70e273127aa8766e4
Codeholic <irenic.tahir@gmail.com>
2023-05-03 13:25:39 +0500

    Cached form validations

--- app/Http/Requests/Form/StoreRequest.php
+++ app/Http/Requests/Form/StoreRequest.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Requests\Form;
 
 use App\Actions\Form\ForceFormRequestFieldsAction;
@@ -8,6 +7,7 @@ use App\Core\Http\Requests\CoreFormRequest;
 use App\Http\Requests\Params\Form\StoreRequestParams;
 use App\Models\Form;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Cache;
 
 class StoreRequest extends CoreFormRequest
 {
@@ -15,28 +15,40 @@ class StoreRequest extends CoreFormRequest
     protected array $validationArr = [];
 
     protected string $formName = "form1";
+
     public function rules(ValidateFormFieldsAction $formAction): array
     {
-        //Extract validations and error messages
-        if (isset($this->input()['formName'])) {
-            $this->formName = $this->input()['formName'];
+        // Cache the form validations
+        $cacheKey = 'form_validations_' . $this->formName;
+        $this->validationArr = Cache::rememberForever($cacheKey, function () use ($formAction) {
+            return $formAction->execute($this->formName);
+        });
+
+        // Extract validations and error messages
+        if ($this->filled('formName')) {
+            $this->formName = $this->input('formName');
+            $cacheKey = 'form_validations_' . $this->formName;
+            $this->validationArr = Cache::rememberForever($cacheKey, function () use ($formAction) {
+                return $formAction->execute($this->formName);
+            });
         }
-        $this->validationArr = $formAction->execute($this->formName);
 
         return $this->validationArr["rules_array"];
     }
 
     public function all($keys = null)
     {
-
         $data = parent::all($keys);
         $form = $this->formName === "form1" ? Form::getForm1() : Form::getForm2();
-        //        force populate fields that are not supplied!
-        foreach($form as $field){
-           if(!isset($data["fields"][$field])) $data["fields"][$field] = null;
+
+        // Force populate fields that are not supplied!
+        foreach ($form as $field) {
+            if (!array_key_exists($field, $data["fields"])) {
+                $data["fields"][$field] = null;
+            }
         }
 
         return $data;
     }
-
 }
+
